566b5b8f17a677ba6ea043619082ebc5e25e0488
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-24 19:17:47 -0400

    updates for Heroku push

--- EFSblog/wsgi.py
+++ EFSblog/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EFSblog.settings")
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
