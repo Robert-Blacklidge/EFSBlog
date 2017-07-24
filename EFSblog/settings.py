481045f5b1b9a14a8af2c7f5b917d559d9cb922d
Robert-Blacklidge <robertm.blacklidge@gmail.com>
2017-07-24 19:07:03 -0400

    removed dir in templates setting for Heroku push

--- EFSblog/settings.py
+++ EFSblog/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'EFSblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
